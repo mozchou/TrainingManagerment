2245ec8a63d3ae7bc6ac2542f36234708dc26d9d
mozchou <leminhchau5112@gmail.com>
2023-12-13 04:50:53 +0700

    Mô tả commit của bạn

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -16,34 +16,6 @@ const dbConfig = {
     }
 }
 
-// const connection = () => {
-
-//     sql.connect(dbConfig).then(function (err) {
-//         var sqlrequest = new sql.Request();
-//         let result = sqlrequest.query("select * from Users").then(function (recordset) {
-//             sql.close(function (value) {
-//                 console.log("connection6 closed");
-//             });
-//             console.log(recordset);
-//             recordset.recordset.forEach(function (record) {
-//                 console.log("ID User:", record.id_user);
-//                 console.log("Username:", record.username);
-//                 console.log("Password:", record.passwrd);
-//                 console.log("ID QuyenTK:", record.id_quyenTK);
-//                 console.log("--------------");
-//             });
-
-//         }).catch(function (err) {
-//             console.log(err);
-//         });
-//         console.log(result);
-//     }).catch(function (err) {
-//         console.log(err);
-//     });
-
-
-// };
-
 
 
 export default dbConfig;
